0f152ac283860223beb381d41448a91cf4940acc
Elys_Sanntos <elys.santos@hotmail.com>
2022-07-08 14:39:50 -0300

    Primeiro commit

---
+++ DIO/Java/Projetos/Eclipse/Primeiro Programa Java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+
+	}
+
+}
