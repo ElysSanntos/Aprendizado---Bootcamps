a9f291500acf6c757970155fe9235e1317aa38b7
Elys_Sanntos <elys.santos@hotmail.com>
2022-07-08 17:34:47 -0300

    Primeiro commit via Intelijei

---
+++ DIO/Java/Projetos/Intelijei/Primeiro Programa Intelijei/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+         /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! "+ (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
+
+
+
+
