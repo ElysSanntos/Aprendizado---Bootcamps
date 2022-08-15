ecf1a14ddaad0f7c82869e632a28dc1a63a74fb1
Elys_Sanntos <elys.santos@hotmail.com>
2022-08-15 18:06:27 -0300

    Campo de texto

--- Alura/Formação Front-end/ReactJS/organo/src/App.js
+++ Alura/Formação Front-end/ReactJS/organo/src/App.js
@@ -1,10 +1,12 @@
 
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto'
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto />
    
     </div>
   );
---
+++ Alura/Formação Front-end/ReactJS/organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css'
+
+const CampoTexto = ()=>{
+
+    return(
+<div className = "campo-texto">
+    <label>Nome</label>
+    <input placeholder = "Digite seu nome"/>
+</div>
+    );
+}
+
+export default CampoTexto;
