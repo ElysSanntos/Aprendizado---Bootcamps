84cbebda76cabdf5f2673cc7228b99715a3d7130
Elys_Sanntos <elys.santos@hotmail.com>
2022-08-15 10:24:30 -0300

    Primeiro componente: Banner

---
+++ Alura/Formação Front-end/ReactJS/organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner(){
+    //JSX
+return (
+  <header className = "banner">
+    <img src="/imagens/banner.png" alt="Banner principal da página do Organo" />
+  </header>
+);
+}
+
+export default Banner
