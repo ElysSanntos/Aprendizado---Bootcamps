4546cdc099788f395ca8be1ada392ce5c7b543d7
Elys_Sanntos <elys.santos@hotmail.com>
2022-08-22 10:54:04 -0300

    Desafios Iniciais de Pilha: Data de entrega 22/08/22 1) Desenvolver o método clonar() na classe Pilha 2) Desenvolver o método inverter() na classe Pilha (inverte a própria pilha) 3) Desenvolver o método inverter() FORA da classe Pilha (gerar uma nova pilha com os elementos da pilha do parâmetro, invertida)
    Variações de inverter, caso queiram:
    4) Desenvolver o método inverter() FORA da classe Pilha (inverter a pilha do parâmetro)

--- COTUCA/Estrutura_De_Dados_I/Pilha.java
+++ COTUCA/Estrutura_De_Dados_I/Pilha.java
@@ -40,11 +40,27 @@ public class Pilha{
     public boolean vazia(){
        return (topo == -1);
     }
-    public void inverter (){}
-    public Pilha clonar(){
-        return null;
-    }
-    public String toString(){
+
+
+    //1) Desenvolver o método clonar() na classe Pilha
+    public Pilha clone() {
+        Pilha aux = new Pilha( this.Vetor.length );
+
+        if (!PilhaVazia()){
+            for (int i = 0; i<= this.topo;i++)
+                aux.Vetor[i] = this.Vetor[i];
+
+            aux.topo = this.topo;
+
+    //2) Desenvolver o método inverter() na classe Pilha (inverte a própria pilha
+      public void inverter (){}
+
+    //3) Desenvolver o método inverter() FORA da classe Pilha (gerar uma nova pilha com os elementos da pilha do parâmetro, invertida)
+
+    //Variações de inverter, caso queiram:
+    //4) Desenvolver o método inverter() FORA da classe Pilha (inverter a pilha do parâmetro)
+
+            public String toString(){
        String ret="Topo --> [ ";
 
        for (int i=topo; i>-1 ; i--){
