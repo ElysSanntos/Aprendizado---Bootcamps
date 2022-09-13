af8cbfd496387067a0e049c70a61a18ce9b7df03
Elys Sanntos <elys.santos@hotmail.com>
2022-09-13 15:35:59 -0300

    Criação dos verbos HHTP -- Até aqui não consegui rodar o BD nem o Swagger

--- COTUCA/Desenv_WebIII/.NET Core EF com SQLServer e MySQL/ProjetoEscola_API/Controllers/AlunoController.cs
+++ COTUCA/Desenv_WebIII/.NET Core EF com SQLServer e MySQL/ProjetoEscola_API/Controllers/AlunoController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoEscola_API.Data;
 using ProjetoEscola_API.Models;
-
 
 namespace ProjetoEscola_API.Controllers
 {
@@ -25,6 +28,96 @@ namespace ProjetoEscola_API.Controllers
             {
                 return _context.Aluno.ToList();
             }
-        }
+            [HttpGet("{AlunoId}")]
+            public ActionResult<List<Aluno>> Get(int AlunoId)
+            {
+                try
+                {
+                    var result = _context.Aluno.Find(AlunoId);
+                    if (result == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(result);
+                }
+                catch
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+                }
+            }
+            [HttpPost]
+            /*
+            Através do atributo [HttpPost] informamos que esse método será acessado por uma requisição POST.
+            Alteramos a declaração do método, para que funcione de forma assíncrona
+            */
+            public async Task<ActionResult> post(Aluno model)
+            {
+                try
+                {
+                _context.Aluno.Add(model);
+                if (await _context.SaveChangesAsync() == 1)
+                {
+                //return Ok();
+                return Created($"/api/aluno/{model.RA}", model);
+                        }
+                    }
+                catch
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+                }
+
+                    // retorna BadRequest se não conseguiu incluir
+                    return BadRequest();
+             }
+
+
+            [HttpPut("{AlunoId}")]
+            public async Task<IActionResult> put(int AlunoId, Aluno dadosAlunoAlt)
+            {
+                try
+                {
+                    //verifica se existe aluno a ser alterado
+                    var result = await _context.Aluno.FindAsync(AlunoId);
+                    if (AlunoId != result.id)
+                    {
+                        return BadRequest();
+                    }
+                    result.ra = dadosAlunoAlt.ra;
+                    result.nome = dadosAlunoAlt.nome;
+                    result.codCurso = dadosAlunoAlt.codCurso;
+                    await _context.SaveChangesAsync();
+                    return Created($"/api/aluno/{dadosAlunoAlt.ra}", dadosAlunoAlt);
+                }
+                catch
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    "Falha no acesso ao banco de dados.");
+                }
+            }
+
+
+            [HttpDelete("{AlunoId}")]
+            public async Task<ActionResult> delete(int AlunoId)
+            {
+                try
+                {
+                    //verifica se existe aluno a ser excluído
+                    var aluno = await _context.Aluno.FindAsync(AlunoId);
+                    if (aluno == null)
+                    {
+                        //método do EF
+                        return NotFound();
+                    }
+                    _context.Remove(aluno);
+                    await _context.SaveChangesAsync();
+                    return NoContent();
+                }
+                catch
+                {
+                    return this.StatusCode(StatusCodes.Status500InternalServerError, 
+                    "Falha no acesso ao banco de dados.");
+                }
+            }
     }
+  }
 }
