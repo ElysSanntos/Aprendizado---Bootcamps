14cbc4b0ec715984c3310f214e680648eb0390d8
Elys Sanntos <elys.santos@hotmail.com>
2022-09-13 09:05:25 -0300

    Exercicio relogio - ainda dá estouro de hora - corrigir

---
+++ GRANDE_PORTE/Java MF - BRQ - SET22/Aula4_220912/src/Relogio/Main.java
@@ -0,0 +1,38 @@
+package Relogio;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+    Relogio cassio  = new Relogio();
+    Relogio oriente  = new Relogio(10,30,10);
+    
+    cassio.setMarca("Cassio");
+    cassio.setHora(12);
+    cassio.setMinuto(20);
+    cassio.setSegundo(30);
+    
+    oriente.setMarca("Oriente");
+    
+    System.out.println("======Hora atual======\n");
+    cassio.exibeHorario();
+    oriente.exibeHorario();
+    
+    System.out.println("\n====Hora adiantada====\n");
+    cassio.adiantar(40);;
+    cassio.exibeHorario();
+    
+    oriente.adiantar(30);
+    oriente.exibeHorario();
+    
+    System.out.println("\n=====Hora atrasada====\n");
+    cassio.atrasar(40);;
+    cassio.exibeHorario();
+    
+    oriente.atrasar(30);
+    oriente.exibeHorario();
+    
+    
+	}
+
+}
---
+++ GRANDE_PORTE/Java MF - BRQ - SET22/Aula4_220912/src/Relogio/Relogio.java
@@ -0,0 +1,93 @@
+package Relogio;
+
+public class Relogio {
+	//ATRIBUTOS
+	
+	private int segundo;
+	private int minuto;
+	private int hora;
+	public String marca;
+	
+	 
+
+	 
+	//CONSTRUTOR
+		public Relogio() {};
+				
+		public Relogio(int hora, int minuto, int segundo) {
+			this.hora = hora;
+			this.minuto = minuto;
+			this.segundo = segundo;
+			//this.marca = marca;
+		}
+
+	 
+	//GETTERS E SETTERS
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(int minuto) {
+		this.minuto = minuto;
+	}
+
+	public int getSegundo() {
+		return segundo;
+	}
+
+	public void setSegundo(int segundo) {
+		this.segundo = segundo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	//METODOS PROPRIOS DA CLASSE
+	
+	/**
+	 * Adianta o horário em segundos
+	 * qtdSegundos == Segundos para adiantar
+	 */
+		 
+	public void adiantar(int qtdSegundo) {
+		
+		int segundosAgora = (this.hora * 3600)
+				+ (this.minuto * 60) + this.segundo + qtdSegundo;
+		
+		this.hora = segundosAgora / 3600;
+		this.minuto = (segundosAgora / 60) % 60;
+		this.segundo = segundosAgora % 60;
+		
+	}
+	/**
+	 * Atrasa o horário em segundos
+	 * qtdSegundos Segundos para atrasar
+	 */
+	public void atrasar(int qtdSegundos) {
+		
+		int segundosAgora = ((this.hora * 3600) + (this.minuto * 60) 
+				+ this.segundo) - qtdSegundos;
+		
+		this.hora = segundosAgora / 3600;
+		this.minuto = (segundosAgora / 60) % 60;
+		this.segundo = segundosAgora % 60;
+	}
+	public void exibeHorario() {
+		System.out.println(marca +"\t"+ hora + ":" + minuto + ":" + segundo);
+	}
+
+}
