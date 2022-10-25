16013b90b1373f1ddd4e86d79f91469a6c1ecce6
Elys_Sanntos <elys.santos@hotmail.com>
2022-10-24 21:56:50 -0300

    Aula 21 - 24/10/2022 - Fabrizio - Arquitetura: # Objetivo da Aula                                                - Aprender mais uma anotation                                                - Introdução a Regex                                                - Manipular msgs de erros                                                - Status code

--- GRANDE_PORTE/Java MF - BRQ - SET22/Arquitetura/ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
+++ GRANDE_PORTE/Java MF - BRQ - SET22/Arquitetura/ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
@@ -4,10 +4,12 @@ import com.brq.ms01.models.UsuarioModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.modelmapper.ModelMapper;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
@@ -18,8 +20,10 @@ public class UsuarioDTO {
 
 	@NotEmpty(message = "Não pode enviar o nome em branco")
 	@NotNull(message = "Preenchimento obrigatório")
+	@Length(min = 3,max = 40,message = "A qtde de caracteres deve estar no range de 3 a 50")
 	private String nome;
 	private String email;
+	@Pattern( regexp ="^\\([1-9]{2}\\) (?:[2-8]|9[1-9])[0-9]{3}\\-[0-9]{4}$" ,message = "Telefone inválido")
 	private String telefone;
 
 
