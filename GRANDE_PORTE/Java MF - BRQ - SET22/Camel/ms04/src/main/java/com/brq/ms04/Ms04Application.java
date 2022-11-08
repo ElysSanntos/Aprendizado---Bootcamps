7d9fea2584fbcf7c7a486a03ec08fd0b414c7160
Elys_Sanntos <elys.santos@hotmail.com>
2022-11-07 23:24:27 -0300

    Aula32 07/11/2022 - Fabrizio - Segunda: Inicio do aprendizado com Camel - Criação da primeira rota.

---
+++ GRANDE_PORTE/Java MF - BRQ - SET22/Camel/ms04/src/main/java/com/brq/ms04/Ms04Application.java
@@ -0,0 +1,13 @@
+package com.brq.ms04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms04Application.class, args);
+	}
+
+}
---
+++ GRANDE_PORTE/Java MF - BRQ - SET22/Camel/ms04/src/main/java/com/brq/ms04/routes/CopyFilesRoute.java
@@ -0,0 +1,18 @@
+package com.brq.ms04.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CopyFilesRoute extends RouteBuilder  {
+
+	@Override
+	public  void configure() throws Exception{
+
+		/*A rota é do tipo arquivo e parte da pasta input*/
+		from("file:input")
+				/*A rota é do tipo arquivo e tem com odestino a paste output*/
+				.to("file:output");
+
+	}
+}
