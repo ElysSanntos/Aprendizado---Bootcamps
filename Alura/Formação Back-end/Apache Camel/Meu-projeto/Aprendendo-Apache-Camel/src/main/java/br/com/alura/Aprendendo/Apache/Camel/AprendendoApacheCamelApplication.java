c7980b67a3c8e4df36bfdd58caaafe85fdd611e2
Elys Sanntos <elys.santos@hotmail.com>
2022-11-29 18:33:24 -0300

    Meu projeto Camel

---
+++ Alura/Formação Back-end/Apache Camel/Meu-projeto/Aprendendo-Apache-Camel/src/main/java/br/com/alura/Aprendendo/Apache/Camel/AprendendoApacheCamelApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.Aprendendo.Apache.Camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApacheCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApacheCamelApplication.class, args);
+	}
+
+}
