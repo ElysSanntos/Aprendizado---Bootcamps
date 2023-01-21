95415426e0603e8f5e105a4f697b9391f0212ec3
Elys Sanntos <elys.santos@hotmail.com>
2023-01-21 17:35:37 -0300

    Módulo 6: que existe um bloco finally, útil para o fechamento de recursos (como conexão); quando há um bloco finally o bloco catch é opcional; que o bloco finally é sempre executado, sem ou com exceção; como usar o try-with-resources.

---
+++ Alura/PROGRAMAÇÃO/Java/Aplicando Exceções/Projetos-finais/bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+
+public class SaldoInsuficienteException extends Exception{
+	
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ Alura/PROGRAMAÇÃO/Java/Aplicando Exceções/Projetos-finais/bytebank-herdado-conta/src/TesteSaca.java
@@ -0,0 +1,19 @@
+
+public class TesteSaca {
+
+	public static void main(String[] args) {
+		Conta conta = new ContaCorrente(123, 321);
+		
+		conta.deposita(200.0);
+		
+		try {
+			conta.saca(210.0);
+		} catch(SaldoInsuficienteException ex) {
+			System.out.println("Ex: " + ex.getMessage());
+		}
+		
+		System.out.println(conta.getSaldo());
+
+	}
+
+}
