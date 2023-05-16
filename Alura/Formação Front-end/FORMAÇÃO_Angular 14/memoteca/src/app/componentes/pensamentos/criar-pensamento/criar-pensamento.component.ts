e8e42e3b09d96f9eb35f128457fe2b51a4912157
Elys Sanntos <elys.santos@hotmail.com>
2023-05-16 17:02:56 -0300

    Property Binding

---
+++ Alura/Formação Front-end/FORMAÇÃO_Angular 14/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.scss']
+})
+export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria:'Dev',
+    modelo:''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
