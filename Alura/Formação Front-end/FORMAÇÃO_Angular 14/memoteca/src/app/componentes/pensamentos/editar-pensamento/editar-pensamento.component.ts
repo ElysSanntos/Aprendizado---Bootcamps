f5c2fc201203f87db96d80673f470ec838d1a003
Elys Sanntos <elys.santos@hotmail.com>
2023-05-18 19:41:29 -0300

    Componente Editar

---
+++ Alura/Formação Front-end/FORMAÇÃO_Angular 14/memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento/pensamento';
+
+@Component({
+  selector: 'app-editar-pensamento',
+  templateUrl: './editar-pensamento.component.html',
+  styleUrls: ['./editar-pensamento.component.scss']
+})
+export class EditarPensamentoComponent implements OnInit {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  editarPensamento() {
+    this.service.editar(this.pensamento).subscribe(() =>
+      this.router.navigate(['/listarPensamento']))
+
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+}
